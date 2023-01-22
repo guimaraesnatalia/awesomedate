530e5a23b46910c432916f50b5ade6a73bea27c5
guimaraesnatalia <nataliasouzaguimaraes@gmail.com>
2023-01-22 11:06:59 -0300

    Update: suggestion message

--- main.py
+++ main.py
@@ -11,14 +11,26 @@ options = Options()
 dates = Dates()
 
 
+def suggest_place(message):
+    place = dates.getADate(options.getWether(), options.getCosts())
+
+    text = """
+SUGESTÃO:
+   Talvez """ + place + """ pode ser uma boa opção.
+
+
+O que acham?
+    /Love_it Amei, iremos visitar!
+    /Visited Já visitamos este lugar
+    /Not_now Não estamos afim de fazer esse passeio agora"""
+    bot.send_message(message.chat.id, text)
+
+
 @bot.message_handler(commands=["Sun", "Some_clouds", "Rain", "At_night"])
 def choose_wether(message):
     option_selected = message.text[1:]
     options.setWether(option_selected)
-    place = dates.getADate(options.getWether(), options.getCosts())
-
-    text = " Talvez " + place + " pode ser uma boa opção"
-    bot.send_message(message.chat.id, text)
+    suggest_place(message)
 
 
 @bot.message_handler(commands=["No_money", "Some_money", "Richer_than_richie_rich"])
@@ -26,11 +38,11 @@ def choose_costs(message):
     option_selected = message.text[1:]
     options.setCosts(option_selected)
     text = """
-    Como está o tempo por ai?
-        /Sun Ensolarado 🌞
-        /Some_clouds Algumas nuvens ⛅️
-        /Rain Chovendo 🌧
-        /At_night É noite 🌜"""
+Como está o tempo por ai?
+    /Sun Ensolarado 🌞
+    /Some_clouds Algumas nuvens ⛅️
+    /Rain Chovendo 🌧
+    /At_night É noite 🌜"""
     bot.send_message(message.chat.id, text)
 
 
@@ -48,7 +60,7 @@ Quanto vocês pretendem gastar?
      /No_money Nenhum dinheiro 💸
      /Some_money Pouco dinheiro 💵
      /Richer_than_richie_rich Bastante 💰
-     
+
 Responder qualquer outra coisa não vai funcionar, clique em uma das opções"""
     bot.reply_to(message, text)
 
